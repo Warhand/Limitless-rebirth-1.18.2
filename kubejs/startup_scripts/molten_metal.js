612c398798b6a5573aadfd36bb0ad1206fff42cb
Rosie-Holstein <creeper620@gmail.com>
2023-01-13 05:24:39 -0500

    KubeJS and assets added
    KubeJS added, as it allows me to easily add in custom molten metal.

---
+++ kubejs/startup_scripts/molten_metal.js
@@ -0,0 +1,119 @@
+onEvent('fluid.registry', event => {
+//molten iron
+    event.create('molten_iron')
+        .thickTexture(0xE65B1E)
+        .bucketColor(0xE65B1E)
+		.displayName('Molten iron')
+
+//molten gold
+    event.create('molten_gold')
+        .thickTexture(0xFDDD10)
+        .bucketColor(0xFDDD10)
+		.displayName('Molten gold')
+
+//molten copper
+    event.create('molten_copper')
+        .thickTexture(0xE8810D)
+        .bucketColor(0xE8810D)
+		.displayName('Molten copper')
+
+//molten zinc
+    event.create('molten_zinc')
+        .thickTexture(0xE1FBDB)
+        .bucketColor(0xE1FBDB)
+		.displayName('Molten zinc')
+
+//molten tin
+    event.create('molten_tin')
+        .thickTexture(0xA9E0E1)
+        .bucketColor(0xA9E0E1)
+		.displayName('Molten tin')
+
+//molten steel
+    event.create('molten_steel')
+        .thickTexture(0x635D56)
+        .bucketColor(0x635D56)
+		.displayName('Molten steel')
+
+//molten forgotten metal
+    event.create('molten_forgotten_metal')
+        .thickTexture(0x4BFFA0)
+        .bucketColor(0x4BFFA0)
+		.displayName('Molten forgotten metal')
+
+//molten froststeel
+    event.create('molten_froststeel')
+        .thickTexture(0x86C9D1)
+        .bucketColor(0x86C9D1)
+		.displayName('Molten froststeel')
+
+//molten cloggrum
+    event.create('molten_cloggrum')
+        .thickTexture(0xB9AA87)
+        .bucketColor(0xB9AA87)
+		.displayName('Molten cloggrum')
+
+//molten Iridium
+    event.create('molten_iridium')
+        .thickTexture(0xE6FFFA)
+        .bucketColor(0xE6FFFA)
+		.displayName('Molten iridium')
+
+//molten aluminum
+    event.create('molten_aluminum')
+        .thickTexture(0xFFD1B2)
+        .bucketColor(0xFFD1B2)
+		.displayName('Molten aluninum')
+
+//molten netherite
+    event.create('molten_netherite')
+        .thickTexture(0x3F3D3B)
+        .bucketColor(0x3F3D3B)
+		.displayName('Molten netherite')
+
+//molten lead
+
+    event.create('molten_lead')
+        .thickTexture(0x51689F)
+        .bucketColor(0x51689F)
+		.displayName('Molten lead')
+
+//molten hellforged
+
+    event.create('molten_hellforged')
+        .thickTexture(0x90F4F4)
+        .bucketColor(0x90F4F4)
+		.displayName('Molten hellforged')
+
+//molten electrum
+
+    event.create('molten_electrum')
+        .thickTexture(0xF7FA9D)
+        .bucketColor(0xF7FA9D)
+		.displayName('Molten electrum')
+
+//molten enderium
+
+    event.create('molten_enderium')
+        .thickTexture(0x2D644C)
+        .bucketColor(0x2D644C)
+		.displayName('Molten enderium')
+
+//molten bronze
+    event.create('molten_bronze')
+        .thickTexture(0x967222)
+        .bucketColor(0x967222)
+		.displayName('Molten bronze')
+
+//molten constantan
+    event.create('molten_constantan')
+        .thickTexture(0xFC8D58)
+        .bucketColor(0xFC8D58)
+		.displayName('Molten constantan')
+
+//molten brass
+    event.create('molten_brass')
+        .thickTexture(0xE8C660)
+        .bucketColor(0xE8C660)
+		.displayName('Molten brass')
+})
