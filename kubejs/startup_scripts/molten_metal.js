ae8c480e94c84a93ce558f12147366f279830cc8
Rosie-Holstein <creeper620@gmail.com>
2023-01-31 02:29:24 -0500

    Update molten_metal.js
    Added molten metals:
    Uranium,
    Invar,
    Lumium,
    Pendorite,
    Terrasteel,
    Manasteel,
    Elementium,
    Neptunium,
    Hallowed gold,
    Soulstained steel,
    Nickel,
    Silver

--- kubejs/startup_scripts/molten_metal.js
+++ kubejs/startup_scripts/molten_metal.js
@@ -1,119 +1,224 @@
 onEvent('fluid.registry', event => {
 //molten iron
     event.create('molten_iron')
-        .thickTexture(0xE65B1E)
-        .bucketColor(0xE65B1E)
-		.displayName('Molten iron')
+        .thickTexture(0xBA2019)
+        .bucketColor(0xBA2019)
+		.displayName('Molten Iron')
+		.luminosity(15)
 
 //molten gold
     event.create('molten_gold')
-        .thickTexture(0xFDDD10)
-        .bucketColor(0xFDDD10)
-		.displayName('Molten gold')
+        .thickTexture(0xF1EE00)
+        .bucketColor(0xF1EE00)
+		.displayName('Molten Gold')
+		.luminosity(15)
 
 //molten copper
     event.create('molten_copper')
-        .thickTexture(0xE8810D)
-        .bucketColor(0xE8810D)
-		.displayName('Molten copper')
+        .thickTexture(0xF7961C)
+        .bucketColor(0xF7961C)
+		.displayName('Molten Copper')
+		.luminosity(15)
 
 //molten zinc
     event.create('molten_zinc')
-        .thickTexture(0xE1FBDB)
-        .bucketColor(0xE1FBDB)
-		.displayName('Molten zinc')
+        .thickTexture(0xB8FFB3)
+        .bucketColor(0xB8FFB3)
+		.displayName('Molten Zinc')
+		.luminosity(15)
 
 //molten tin
     event.create('molten_tin')
-        .thickTexture(0xA9E0E1)
-        .bucketColor(0xA9E0E1)
-		.displayName('Molten tin')
-
+        .thickTexture(0x8FC8D3)
+        .bucketColor(0x8FC8D3)
+		.displayName('Molten Tin')
+		.luminosity(15)
+		
+//molten nickel
+    event.create('molten_nickel')
+        .thickTexture(0xDCD191)
+        .bucketColor(0xDCD191)
+		.displayName('Molten Nickel')
+		.luminosity(15)
+		
+//molten silver
+    event.create('molten_silver')
+        .thickTexture(0xC3D5D5)
+        .bucketColor(0xC3D5D5)
+		.displayName('Molten Silver')
+		.luminosity(15)
+		
 //molten steel
     event.create('molten_steel')
-        .thickTexture(0x635D56)
-        .bucketColor(0x635D56)
-		.displayName('Molten steel')
+        .thickTexture(0xBFBFBF)
+        .bucketColor(0xBFBFBF)
+		.displayName('Molten Steel')
+		.luminosity(15)
 
 //molten forgotten metal
     event.create('molten_forgotten_metal')
-        .thickTexture(0x4BFFA0)
-        .bucketColor(0x4BFFA0)
-		.displayName('Molten forgotten metal')
+        .thickTexture(0x57e3b3)
+        .bucketColor(0x57e3b3)
+		.displayName('Molten Forgotten Metal')
+		.luminosity(15)
 
 //molten froststeel
     event.create('molten_froststeel')
-        .thickTexture(0x86C9D1)
-        .bucketColor(0x86C9D1)
-		.displayName('Molten froststeel')
+        .thickTexture(0x90B9E0)
+        .bucketColor(0x90B9E0)
+		.displayName('Molten Froststeel')
+		.luminosity(15)
 
 //molten cloggrum
     event.create('molten_cloggrum')
-        .thickTexture(0xB9AA87)
-        .bucketColor(0xB9AA87)
-		.displayName('Molten cloggrum')
+        .thickTexture(0x968168)
+        .bucketColor(0x968168)
+		.displayName('Molten Cloggrum')
+		.luminosity(15)
 
 //molten Iridium
     event.create('molten_iridium')
-        .thickTexture(0xE6FFFA)
-        .bucketColor(0xE6FFFA)
-		.displayName('Molten iridium')
+        .thickTexture(0xDBFEFE)
+        .bucketColor(0xDBFEFE)
+		.displayName('Molten Iridium')
+		.luminosity(15)
 
 //molten aluminum
     event.create('molten_aluminum')
         .thickTexture(0xFFD1B2)
         .bucketColor(0xFFD1B2)
-		.displayName('Molten aluninum')
-
+		.displayName('Molten Aluminum')
+		.luminosity(15)
+		
 //molten netherite
     event.create('molten_netherite')
-        .thickTexture(0x3F3D3B)
-        .bucketColor(0x3F3D3B)
-		.displayName('Molten netherite')
-
+        .thickTexture(0x614D49)
+        .bucketColor(0x614D49)
+		.displayName('Molten Netherite')
+		.luminosity(15)
+		
 //molten lead
-
     event.create('molten_lead')
-        .thickTexture(0x51689F)
-        .bucketColor(0x51689F)
-		.displayName('Molten lead')
-
+        .thickTexture(0x4A74AD)
+        .bucketColor(0x4A74AD)
+		.displayName('Molten Lead')
+		.luminosity(15)
+		
 //molten hellforged
-
     event.create('molten_hellforged')
-        .thickTexture(0x90F4F4)
-        .bucketColor(0x90F4F4)
-		.displayName('Molten hellforged')
-
+        .thickTexture(0xABFAFE)
+        .bucketColor(0xABFAFE)
+		.displayName('Molten Hellforged')
+		.luminosity(15)
+		
 //molten electrum
-
     event.create('molten_electrum')
         .thickTexture(0xF7FA9D)
         .bucketColor(0xF7FA9D)
-		.displayName('Molten electrum')
-
+		.displayName('Molten Electrum')
+		.luminosity(15)
+		
 //molten enderium
-
     event.create('molten_enderium')
-        .thickTexture(0x2D644C)
-        .bucketColor(0x2D644C)
-		.displayName('Molten enderium')
+        .thickTexture(0x11915B)
+        .bucketColor(0x11915B)
+		.displayName('Molten Enderium')
+		.luminosity(15)
 
 //molten bronze
     event.create('molten_bronze')
         .thickTexture(0x967222)
         .bucketColor(0x967222)
-		.displayName('Molten bronze')
+		.displayName('Molten Bronze')
+		.luminosity(15)
 
 //molten constantan
     event.create('molten_constantan')
-        .thickTexture(0xFC8D58)
-        .bucketColor(0xFC8D58)
-		.displayName('Molten constantan')
+        .thickTexture(0xF38C62)
+        .bucketColor(0xF38C62)
+		.displayName('Molten Constantan')
+		.luminosity(15)
 
 //molten brass
     event.create('molten_brass')
-        .thickTexture(0xE8C660)
-        .bucketColor(0xE8C660)
-		.displayName('Molten brass')
+        .thickTexture(0xEDBF36)
+        .bucketColor(0xEDBF36)
+		.displayName('Molten Brass')
+
+//molten Uranium
+    event.create('molten_uranium')
+        .thickTexture(0x446344)
+        .bucketColor(0x446344)
+		.displayName('Molten Uranium')
+		.luminosity(15)
+
+//molten invar
+    event.create('molten_invar')
+        .thickTexture(0xBFD5D1)
+        .bucketColor(0xBFD5D1)
+		.displayName('Molten Invar')
+		.luminosity(15)
+
+//molten signalum
+    event.create('molten_signalum')
+        .thickTexture(0xFF0C00)
+        .bucketColor(0xFF0C00)
+		.displayName('Molten Signalum')
+		.luminosity(15)
+
+//molten lumium
+    event.create('molten_lumium')
+        .thickTexture(0xFFFFB3)
+        .bucketColor(0xFFFFB3)
+		.displayName('Molten Lumium')
+		.luminosity(15)
+
+//molten Pendorite
+    event.create('molten_pendorite')
+        .thickTexture(0x9B65CB)
+        .bucketColor(0x9B65CB)
+		.displayName('Molten Pendorite')
+
+//molten terrasteel
+    event.create('molten_terrasteel')
+        .thickTexture(0x66F50E)
+        .bucketColor(0x66F50E)
+		.displayName('Molten Terrasteel')
+		.luminosity(15)
+
+//molten manastel
+    event.create('molten_manasteel')
+        .thickTexture(0x0E9DF5)
+        .bucketColor(0x0E9DF5)
+		.displayName('Molten Manasteel')
+		.luminosity(15)
+
+//molten Elementium
+    event.create('molten_elementium')
+        .thickTexture(0xFFA9E2)
+        .bucketColor(0xFFA9E2)
+		.displayName('Molten Elementium')
+		.luminosity(15)
+
+//molten neptunium
+    event.create('molten_neptunium')
+        .thickTexture(0x00DDB1)
+        .bucketColor(0x00DDB1)
+		.displayName('Molten Neptunium')
+		.luminosity(15)
+
+//molten hallowed gold
+    event.create('molten_hallowed_gold')
+        .thickTexture(0xFFD800)
+        .bucketColor(0xFFD800)
+		.displayName('Molten Hallowed Gold')
+		.luminosity(15)
+
+//molten soulstained steel
+    event.create('molten_soulstained')
+        .thickTexture(0xE2A5FD)
+        .bucketColor(0xE2A5FD)
+		.displayName('Molten Soulstained Steel')
+		.luminosity(15)
+
 })
